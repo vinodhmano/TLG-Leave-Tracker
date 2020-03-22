45d1432ea4939af349106f3cd41e0fb37a83d059
vinodhraj <vinodh.mano@gmail.com>
2020-03-22 13:06:53 -0400

    Authentication added

--- src/app.js
+++ src/app.js
@@ -1,45 +1,33 @@
-const express = require('express')
-require('./database/dbController')
-const userRouter = require('./routers/userRouter')
-const leaveRouter = require('./routers/leaveRouter')
-const Leave = require('./models/leave')
-const hbs = require('hbs')
-const path = require('path')
+const express = require("express");
+require("./database/dbController");
+const userRouter = require("./routers/userRouter");
+const leaveRouter = require("./routers/leaveRouter");
+const hbs = require("hbs");
+const path = require("path");
 
-const app = express()
-const viewFolder = path.join(__dirname,'../templates/views')
-const partialFolder = path.join(__dirname,'../templates/partials')
-const staticFolder = path.join(__dirname,'/utils')
+const app = express();
+const viewFolder = path.join(__dirname, "../templates/views");
+const partialFolder = path.join(__dirname, "../templates/partials");
+const staticFolder = path.join(__dirname, "/utils");
 
-app.use(express.urlencoded())
-app.use(express.static(staticFolder))
-app.use(express.json())
-app.use(userRouter)
-app.use(leaveRouter)
+app.use(express.urlencoded());
+app.use(express.static(staticFolder));
+app.use(express.json());
+app.use(userRouter);
+app.use(leaveRouter);
 
-app.set('view engine', 'hbs')
-app.set('views', viewFolder)
-hbs.registerPartials(partialFolder)
+app.set("view engine", "hbs");
+app.set("views", viewFolder);
+hbs.registerPartials(partialFolder);
 
+app.get("/index", (req, res) => {
+	res.render("index");
+});
 
-app.get('/index', (req, res) => {
-    res.render('index')
-})
-
-app.get('/help', (req, res) => {
-    res.render('help')
-})
+app.get("/help", (req, res) => {
+	res.render("help");
+});
 
 app.listen(4444, () => {
-    console.log('Application is running in port 4444')
-})
-
-const jwt = require('jsonwebtoken')
-
-const main = () => {
-    const token = jwt.sign({id:144725}, "nobodyknowswhatitis", { expiresIn: "2 days" })
-    const isValid = jwt.verify(token,"nobodyknowswhatitis")
-    console.log(isValid)
-}
-
-main()
+	console.log("Application is running in port 4444");
+});
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,21 @@
-const jwt = require('jsonwebtoken')
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-const auth = (req, res, next) => {
-    
-}
+const auth = async (req, res, next) => {
+	try {
+		const token = req.headers.authorization.replace("Bearer ", "");
+		const decoded = await jwt.verify(token, "mysecretkey");
+		const user = await User.findOne({
+			_id: decoded._id,
+			"tokens.token": token
+		});
+		if (!user) throw new Error();
+		req.user = user;
+		req.token = token;
+		next();
+	} catch (error) {
+		res.status(401).send(error);
+	}
+};
 
-module.exports = auth
+module.exports = auth;
